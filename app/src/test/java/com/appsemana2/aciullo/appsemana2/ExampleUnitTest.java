271eaaa4c6422f6bd41ce34a769fe5a131e0caf0
aciullo <andrea.ciullo@hotmail.com>
2017-01-16 11:17:15 -0300

    Initial commit

---
+++ app/src/test/java/com/appsemana2/aciullo/appsemana2/ExampleUnitTest.java
@@ -0,0 +1,15 @@
+package com.appsemana2.aciullo.appsemana2;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
